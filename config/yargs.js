8ce3aae6cba7cba81b8a0e5ca877cfa7b87ae398
Hector <hectorjara92@gmail.com>
2022-12-28 17:55:12 -0300

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,26 @@
+const argv = require('yargs')
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true,
+        describe: 'Es la base de la tabla de multiplicar'
+    })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        default: false,
+    })
+    .option('h', {
+        alias: 'hasta',
+        type: 'number',
+        default: 10,
+    })
+    .check((argv, options) => {
+        if (isNaN(argv.b)) {
+            throw 'La base tiene que ser un número';
+        }
+        return true;
+    })
+    .argv;
+
+module.exports = argv;
